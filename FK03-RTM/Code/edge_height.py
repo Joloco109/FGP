8c99f08e25efd50a27b2fdbda9e3c70fd771a3a3
Jonas Colve <jonas.colve@outlook.de>
2020-03-05 18:33:00 +0100

    Added Filenames to edge_height.py

--- FK03-RTM/Code/edge_height.py
+++ FK03-RTM/Code/edge_height.py
@@ -1,5 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from reader import read_configure_graph
+from scipy.optimize import curve_fit
+
+edge_names = [
+        [ "HOPG_Edge2_100_Graph.txt",
+            "HOPG_Edge2_200_Graph.txt",
+            "HOPG_Edge2_300_Graph.txt"
+            ],
+        [ "HOPG_Edge_100_Graph.txt",
+            "HOPG_Edge_200_Graph.txt",
+            "HOPG_Edge_300_Graph.txt"
+            ],
+    ]
+
+config1_names = [
+        [ "HOPG_Edge2_100_Graph.txt",
+            "HOPG_Edge2_200_Graph.txt",
+            "HOPG_Edge2_300_Graph.txt"
+            ],
+        [ "HOPG_Edge_100_Graph.txt",
+            "HOPG_Edge_200_Graph.txt",
+            "HOPG_Edge_300_Graph.txt"
+            ],
+    ]
 
 def edge_height( graph_name, config_name ):
     graphs = read_configure_graph( graph_name, config_name )
+    for graph, graph_l, graph_r in graphs:
+        graph
 
+
+
+#edge_height( edge_names[0][0], config_names[0][0] )
