536973e51f48850cebfe864b6a0a2255e21f9a50
Jonas Colve <jonas.colve@outlook.de>
2020-03-05 18:39:09 +0100

    Added mca to ROOT Histogram reader

---
+++ T06/Code/reader.py
@@ -0,0 +1,34 @@
+import codecs
+import numpy as np
+from ROOT import TH1I
+
+hist_file = "Data/Röhre/Kupfer.mca"
+hist_file2 = "Data/Röhre/Leer.mca"
+
+class Hist:
+    def read( file_name, name, title, boundries=None ):
+        hist = Hist()
+
+        with codecs.open(file_name, 'r', 'iso-8859-1') as f:
+            content = f.readlines()
+        content = [ x.strip() for x in content]
+        data = []
+        while len(content) > 0:
+            line = content.pop(0)
+            if line == "<<DATA>>":
+                while not content[0] == "<<END>>":
+                    data.append(int(content.pop(0)))
+                content.pop(0)
+
+        if boundries == None:
+            boundries = (0, len(data))
+
+        t_hist = TH1I(name, title, len(data), boundries[0], boundries[1])
+        for i in range(len(data)):
+            t_hist.SetBinContent(i+1, data[i])
+        hist.hist = t_hist
+
+        return hist
+
+    def name(self):
+        return self.hist.GetName()
