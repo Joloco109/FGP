e7d5bd62feb862117f89ac38dc093f16fd9bb9ff
Jonas Colve <jonas.colve@outlook.de>
2020-03-07 21:07:19 +0100

    Final edge analysis

--- FK03-RTM/Code/edge_height.py
+++ FK03-RTM/Code/edge_height.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from reader import read_configure_graph
 from fitting import lin_regression
-from plot import plot_multifit
+from plot import plot_multifit, plot_fit
 import os
 
 data_dir = "Data/HOPG_Edge/"
@@ -29,9 +29,11 @@ config_names = [
             ],
     ]
 
-err = 2e-11
+err = 7e-11
+z_theo = [ 0,
+        2.01e-9 ]
 
-def edge_height( graph_name, config_name, file_name, draw_edge=False ):
+def edge_height( graph_name, config_name, file_name, draw_edge=False, print_paras=False ):
     graphs = read_configure_graph( graph_name, config_name )
     heights = []
     for graph, graph_l, graph_r in graphs:
@@ -48,11 +50,12 @@ def edge_height( graph_name, config_name, file_name, draw_edge=False ):
         pars_r, cov_r = lin_regression( x_r, y_r, err*np.ones(len(x_r)) )
         y_r_mod = lambda x : pars_r[0]*x+pars_r[1]
 
-        print(graph.name)
-        print("Paras:")
-        print("L:\t({:.4e} +- {:.4e})".format(pars_l[0], cov_l[0][0] ))
-        print("R:\t({:.4e} +- {:.4e})".format(pars_r[0], cov_r[0][0] ))
-        print()
+        if print_paras:
+            print(graph.name)
+            print("Paras:")
+            print("L:\t({:.4e} +- {:.4e})".format(pars_l[0], cov_l[0][0] ))
+            print("R:\t({:.4e} +- {:.4e})".format(pars_r[0], cov_r[0][0] ))
+            print()
 
         a = (pars_l[0] + pars_r[0])/2
         da = pars_l[0] - pars_r[0]
@@ -72,7 +75,7 @@ def edge_height( graph_name, config_name, file_name, draw_edge=False ):
         if not os.path.exists("Graphs/"+file_name+"/"):
             os.makedirs("Graphs/"+file_name)
         plot_multifit( x, y, err*np.ones(len(x)), [x_l,x_r, x_z], [y_l_mod,y_r_mod, z_x],
-                "X"+graph.units[1], "Y"+graph.units[1], graph.name.replace("_"," "), directory="Graphs/"+file_name+"/", show=True )
+                "X"+graph.units[1], "Y"+graph.units[1], graph.name.replace("_"," "), directory="Graphs/"+file_name+"/", show=False)
 
         x_l = graph_l.data[0][:-1]
         x_r = graph_r.data[0][0]
@@ -97,4 +100,22 @@ def heights( edge, scale ):
     heights = edge_height( data_dir+edge_names[edge][scale-1], data_dir+config_names[edge][scale-1], config_names[edge][scale-1][:-5])
     print(    "z=")
     for row in heights:
-        print("    ({:.4f} +- {:.4f})nm".format(row[0]*1e9, row[1]*1e9))
+        print("    ({:.2f} +- {:.2f})nm".format(row[0]*1e9, row[1]*1e9))
+
+    return heights
+
+def edge( edge ):
+    height = []
+    for i in range(1, len(edge_names[edge])+1):
+        height += heights( edge, i )
+    z = np.array([ x[0] for x in height ])
+    z_err = np.array([ x[1] for x in height ])
+    n = np.linspace(0,len(z),len(z))
+    z_m = np.mean(z)
+    z_m_err = np.std(z)
+    plot_multifit( n, z, z_err, [n,n], [lambda x: np.ones(len(x))*z_m, lambda x: np.ones(len(x))*z_theo[edge]],
+            "", "", "Z", show=True)
+
+    print("z_theo = {:.3e}nm".format(z_theo[edge]*1e9))
+    print("z = ({:.3e} +- {:.3e})nm".format(z_m*1e9, z_m_err*1e9))
+    print("z-z_theo = {:.2f} sigma".format((z_m-z_theo[edge])/z_m_err))
