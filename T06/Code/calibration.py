f074f9071d31f72fc504482b65c99f48f7e62806
Jonas Colve <jonas.colve@outlook.de>
2020-03-09 20:10:56 +0100

    calibration still in testing

--- T06/Code/calibration.py
+++ T06/Code/calibration.py
@@ -1,6 +1,7 @@
 from reader import Hist
-from peakfinder import Peaks
+from peakfinder import Peaks, peak_fit
 from plot import plot_hist
+from ctypes import c_double
 
 data_dir = "Data/"
 am_dir = "Am/"
@@ -26,16 +27,18 @@ calibration_names = [ [
         ]
     ]
 
+paras = [ ( 5, 1e2 ),
+        ( 5, 1e2 )]
+
 def calibrate( file_number, am ):
-    directory = data_dir + am_dir if am else roe_dir
+    directory = data_dir + (am_dir if am else roe_dir)
     empty = Hist.read( directory + leer_names[ 1 if am else 0 ], "Leer", "Leer" )
     spectrum = Hist.read( directory + calibration_names[ 1 if am else 0 ][file_number], "Spectrum_Noise", "Spectrum_Noise" )
 
     spectrum = Hist( spectrum.hist - empty.hist, "Spectrum", "Spectrum" )
 
-    plot_hist( spectrum )
+    #plot_hist( spectrum )
 
-    peaks = Peaks( spectrum, accuracy=2, peak_height=3e1 )
-    for p in peaks:
-        print(p)
-        input()
+    peaks = peak_fit( "gaus(0) + [3]", spectrum, accuracy=5, peak_height=1e2 )
+
+    plot_hist( spectrum, logy=False )
