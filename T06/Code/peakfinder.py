73b86b9ff789f4293e28bec1496b189339531d6c
Jonas Colve <jonas.colve@outlook.de>
2020-03-10 11:47:50 +0100

    Added cut between peaks of different scale

--- T06/Code/peakfinder.py
+++ T06/Code/peakfinder.py
@@ -57,7 +57,9 @@ class Peaks:
 
         return (lower,upper, maximum, max_val)
 
-def peak_fit( func, hist, accuracy=10, peak_height=None ):
+max_peak_number = 3
+
+def peak_fit( func, hist, accuracy=10, peak_height=1e2, peak_fac = 10 ):
     peaks = [ peak for peak in Peaks( hist, accuracy=accuracy, peak_height=peak_height ) ]
     peaks.sort( key=lambda x : x[0] )
     peaks = [ (
@@ -69,25 +71,31 @@ def peak_fit( func, hist, accuracy=10, peak_height=None ):
     fits = []
     i = 0
     while i < len(peaks):
-        j = 0
+        j = 1
         start, end = peaks[i][:2]
         cur_func = "[0] + gaus(1)"
         max_vals = [ peaks[i][3] ]
         max_pos = [ peaks[i][2] ]
         widths = [ min( peaks[i][1] - peaks[i][2], peaks[i][2]-peaks[i][0]) ]
 
-        while i+1 < len(peaks) and end >= peaks[i+1][0]:
+        while i+1 < len(peaks) and (
+                end >= peaks[i+1][0]
+                ) and (
+                max(max_vals[-1], peaks[i+1][3])/min(max_vals[-1], peaks[i+1][3]) < peak_fac
+                ):
             i += 1
-            j += 1
             end = peaks[i][1]
             cur_func += " + gaus({})".format(3*j+1)
             max_vals.append( peaks[i][3] )
             max_pos.append( peaks[i][2] )
             widths.append( min( peaks[i][1] - peaks[i][2], peaks[i][2]-peaks[i][0]) )
+            j += 1
+            if j >= max_peak_number:
+                break
 
         fit = TF1("peak_{}".format(i), cur_func, start, end)
         fit.SetParameter( 0, 0 )
-        for k in range(j+1):
+        for k in range(j):
             fit.SetParameter( 3*k+1, max_vals[k] )
             fit.SetParLimits( 3*k+1, 0, 10*max_vals[k] )
             fit.SetParameter( 3*k+2, max_pos[k] )
