8b66067178a0b0c0ac048f5e266a86390733f9fd
Jonas Colve <jonas.colve@outlook.de>
2020-03-10 16:37:51 +0100

    Settings for Calibration

--- T06/Code/calibration.py
+++ T06/Code/calibration.py
@@ -29,11 +29,11 @@ calibration_names = [ [
 
 calibration_elements = [ [
             [ "Cu" ],
-            [ "Au" ],
+            [ "Ag" ],
             [ "Fe", "Mo", "Cr", "Ni" ]
         ],[
             [ "Cu" ],
-            [ "Au" ],
+            [ "Ag" ],
             [ "Fe", "Mo", "Cr", "Ni" ],
             [ "Ba" ],
             [ "Mo" ],
@@ -42,10 +42,10 @@ calibration_elements = [ [
         ]
     ]
 
-paras = [ ( 5, 1e2 ),
-        ( 5, 1e2 )]
+paras = [ ( 5, 1.5e2, 5 ),
+        ( 5, 5e1, 5 )]
 
-def calibrate( file_number, am ):
+def calibrate( file_number, am, ohne_leer=True ):
     directory = data_dir + (am_dir if am else roe_dir)
     empty = Hist.read( directory + leer_names[ 1 if am else 0 ], "Leer", "Leer" )
     spectrum = Hist.read( directory + calibration_names[ 1 if am else 0 ][file_number], "Spectrum", "Spectrum" )
@@ -56,16 +56,17 @@ def calibrate( file_number, am ):
     for line in spectral_lines:
         print(line)
 
-    spectrum = Hist( spectrum.hist - empty.hist, "Spectrum", "Spectrum" )
+    if ohne_leer:
+        spectrum = Hist( spectrum.hist - empty.hist, "Spectrum", "Spectrum" )
 
-    peaks = peak_fit( spectrum, accuracy=5, peak_height=1e2 )
+    acc, height, fac = paras[ 1 if am else 0 ]
+    peaks = peak_fit( spectrum, accuracy=acc, peak_height=height, peak_fac=fac)
 
     energies = []
     energy_res = []
 
     for peak in peaks:
         print(peak)
-        print("Chi^2 = {:=7.2f}".format( peak.GetChisquare() ))
         print("C     = {:=8.3f} +- {:=8.3f}".format( peak.GetParameter(0), peak.GetParError(0) ))
         for j in range(0, int((peak.GetNpar()-1)/3) ):
             energies.append( (peak.GetParameter(3*j+2), peak.GetParError(3*j+2)) )
@@ -73,6 +74,8 @@ def calibrate( file_number, am ):
             print("A_{}   = {:=8.3f} +- {:=8.3f}".format( j, peak.GetParameter(3*j+1), peak.GetParError(3*j+1) ))
             print("mu_{}  = {:=8.3f} +- {:=8.3f}".format( j, peak.GetParameter(3*j+2), peak.GetParError(3*j+2) ))
             print("sig_{} = {:=8.3f} +- {:=8.3f}".format( j, peak.GetParameter(3*j+3), peak.GetParError(3*j+3) ))
+        print("Chi^2 = {:=7.2f}".format( peak.GetChisquare() ))
+        print("Chi^2/NDF = {:=7.2f}".format( peak.GetChisquare()/peak.GetNDF() ))
         print()
 
-    plot_hist( spectrum, logy=False )
+    plot_hist( spectrum, logy=False)
