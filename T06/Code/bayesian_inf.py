a7e96f5d0a5da1277e62b7b0f0f04384d1171566
Jonas Colve <jonas.colve@outlook.de>
2020-03-11 16:21:46 +0100

    (bayesion inference) added recording of single measurement probs

--- T06/Code/bayesian_inf.py
+++ T06/Code/bayesian_inf.py
@@ -13,11 +13,17 @@ class ModelDist:
         self.sigma_E = [np.array([ E[1] for E in hypothesis ]) for hypothesis in energies]
         self.Probs = np.array( probs )
         self.pEForH = pEForH
+        self.measurement_probs = []
 
     def Update( self, energy ):
+        p = []
         f = np.zeros(len(self.Probs))
         for eElem, sigma_eElem, i in zip(self.Energies, self.sigma_E, range(len(self.Probs))):
-            f[i] = np.sum( self.pEForH( energy[0], energy[1], eElem, sigma_eElem ) )/ len(eElem)
+            p_e = self.pEForH( energy[0], energy[1], eElem, sigma_eElem )/ len(eElem)
+            f[i] = np.sum( p_e )
+            p.append(p_e)
         self.Probs *= f
-        self.Probs /= sum(self.Probs)
+        c = sum(self.Probs)
+        self.Probs /= c
+        self.measurement_probs.append([ p_e/c for p_e in p ])
         return self
