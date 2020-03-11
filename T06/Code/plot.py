88b6e20e9c0220c13073cfe45ff9588903909278
Jonas Colve <jonas.colve@outlook.de>
2020-03-11 11:22:29 +0100

    Added errorbars to histogramgraphs

--- T06/Code/plot.py
+++ T06/Code/plot.py
@@ -5,7 +5,7 @@ def plot_hist( hist, logy=False ):
     canvas = TCanvas('hist_canvas', 'Histogram', 200, 10, 700, 500)
     if logy :
         canvas.SetLogy()
-    hist.hist.Draw()
+    hist.hist.Draw("E1")
     canvas.Update()
     input()
 
