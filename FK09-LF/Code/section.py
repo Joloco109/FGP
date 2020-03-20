829a36344cb0cbc25e771cb4a5549a1cdaccf5e3
Jonas Colve <jonas.colve@outlook.de>
2020-03-20 13:54:47 +0100

    Fixed Bug in section, now correctly converts indices

--- FK09-LF/Code/section.py
+++ FK09-LF/Code/section.py
@@ -19,7 +19,12 @@ def section( graph, tresh=0.07 ):
 
             r_vals = []
             for v in range(len(vals)):
-                filtered = np.array([ vals[v][j] for j in range(start,i) if T[j] in temps[v] ])
+                filtered = []
+                for j in range(start,i):
+                    (index,) = np.where( np.isclose( temps[v], T[j]) )
+                    if len(index)>0:
+                        filtered.append( vals[v][index[0]] )
+                filtered = np.array( filtered )
                 if len(filtered) > 0:
                     r_vals.append( (np.mean( filtered ), np.std( filtered )) )
                 else:
@@ -31,9 +36,23 @@ def section( graph, tresh=0.07 ):
                 ) )
             start = None
     else :
+        r_vals = []
+        for v in range(len(vals)):
+            filtered = []
+            for j in range(start,len(T)):
+                (index,) = np.where( np.isclose( temps[v], T[j]) )
+                if len(index)>0:
+                    filtered.append( vals[v][index[0]] )
+            filtered = np.array( filtered )
+
+            if len(filtered) > 0:
+                r_vals.append( (np.mean( filtered ), np.std( filtered )) )
+            else:
+                r_vals.append( (None,None) )
+
         new_vals.append( (
                 (np.mean( T[start:len(T)] ), np.std( T[start:len(T)] )),
-                *[ (np.mean( v[start:len(T)] ), np.std( v[start:len(T)] )) for v in vals]
+                *r_vals
             ) )
 
     ret_graph = MultiGraph()
@@ -50,19 +69,26 @@ def section( graph, tresh=0.07 ):
 
 
 def main():
-    print()
+    print("He")
     caliC = calibrate_C()
-    graphHe = MultiGraph.Read( "Data/He.txt", lambda x: caliC.GetX((x[1],0))[0], [None,None,"Cu","Ta","Si",None,None] )
+    print()
+    graphHe = MultiGraph.Read( "Data/He.txt", lambda x: caliC.GetX((x[1],0))[0], [None,None,"Cu (He)","Ta (He)","Si (He)",None,None] )
     sectionHe = section( graphHe, 0.005 )
+
     for g, s in zip( graphHe.subgraphs, sectionHe.subgraphs ):
         g.graph.SetMarkerColor(4)
         g.Draw( options="A*" )
         s.Draw( options="P" )
         input()
+    print()
+    print()
 
+    print("Ni")
     caliPt = calibrate_Pt()
-    graphN = MultiGraph.Read( "Data/N.txt", lambda x: caliPt.GetX((x[0],0))[0], [None,None,"Cu","Ta","Si",None,None] )
+    print()
+    graphN = MultiGraph.Read( "Data/N.txt", lambda x: caliPt.GetX((x[0],0))[0], [None,None,"Cu (N)","Ta (N)","Si (N)",None,None] )
     sectionN = section( graphN, 0.005 )
+
     for g, s in zip( graphN.subgraphs, sectionN.subgraphs ):
         g.graph.SetMarkerColor(4)
         g.Draw( options="A*" )
