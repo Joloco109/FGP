b5115597faefc4ce12b3f80bf595875fea52a789
Jonas Colve <jonas.colve@outlook.de>
2020-03-20 15:59:57 +0100

    Tried potting He and N together

--- FK09-LF/Code/analysis.py
+++ FK09-LF/Code/analysis.py
@@ -1,16 +1,19 @@
-from ROOT import TF1
+from ROOT import TF1, TCanvas
 
 from graph import MultiGraph
 from calibration import calibrate_C, calibrate_Pt
 from function import Function
 from section import section
 
-def draw( graph, funcX=None, funcY=None ):
+def draw( graph, funcX=None, funcY=None, options=None ):
     if not funcY==None:
         graph.Apply( funcY )
     if not funcX==None:
         graph.ApplyX( funcX )
-    graph.Draw()
+    if options==None:
+        graph.Draw()
+    else:
+        graph.Draw( options=options )
 
 
 if __name__=="__main__":
@@ -21,6 +24,10 @@ if __name__=="__main__":
     caliC = calibrate_C()
     graphHe = MultiGraph.Read( "Data/He.txt", lambda x: caliC.GetX((x[1],0))[0], [None,None,"Cu","Ta","Si",None,None] )
     sectionHe = section(graphHe)
+    for g in sectionHe.subgraphs[:2]:
+        draw( g, None, None  )
+        input()
+    '''
 
     cloneHe = sectionHe.Clone()
     for g in cloneHe.subgraphs[:2]:
@@ -30,11 +37,16 @@ if __name__=="__main__":
     for g in cloneHe.subgraphs[:2]:
         draw( g, flog, flog )
         input()
+    '''
 
 
     caliPt = calibrate_Pt()
-    graphN = MultiGraph.Read( "Data/N.txt", lambda x: caliPt.GetX((x[1],0))[0], [None,None,"Cu","Ta","Si",None,None] )
+    graphN = MultiGraph.Read( "Data/N.txt", lambda x: caliPt.GetX((x[0],0))[0], [None,None,"Cu","Ta","Si",None,None] )
     sectionN = section(graphN)
+    for g in sectionN.subgraphs[:2]:
+        draw( g, None, None  )
+        input()
+    '''
 
     cloneN = sectionN.Clone()
     for g in cloneN.subgraphs[:2]:
@@ -43,4 +55,13 @@ if __name__=="__main__":
     cloneN = sectionN.Clone()
     for g in cloneN.subgraphs[:2]:
         draw( g, flog, flog )
+        input()
+    '''
+
+    canvas = TCanvas("canvas","canvas")
+    for gHe, gN in zip( sectionHe.subgraphs, sectionN.subgraphs):
+        gHe.graph.SetMarkerColor(4)
+        gN.graph.SetMarkerColor(6)
+        gHe.Draw( options="AP" )
+        gN.Draw( options="P" )
         input()
