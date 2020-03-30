6e319a5d61143c85271b65ee18f0242680391f16
Jonas Colve <jonas.colve@outlook.de>
2020-03-30 14:38:44 +0200

    Added config.py for file location, fit configs, etc.

---
+++ T02/Code/config.py
@@ -0,0 +1,12 @@
+data_dir = "Data/"
+
+cali_dir = data_dir+"Kalibrierung/"
+
+cali_files = [
+        "137 Cs Kalibrierung.TKA",
+        "Co Kalibrierung.TKA",
+        "Eu Kalibrierung.TKA",
+        "Na 22 Kalibrierung.TKA",
+    ]
+
+cali_rausch = "Rauschen.TKA"
