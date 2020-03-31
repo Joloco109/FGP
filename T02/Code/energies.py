48fe6af5ace84a1945594cc41b8037f8de5c0f3e
Jonas Colve <jonas.colve@outlook.de>
2020-03-31 12:31:24 +0200

    Completed energies.py

--- T02/Code/energies.py
+++ T02/Code/energies.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 
 from config import cali_dir, cali_energy_file
 #from enum import 
@@ -9,11 +10,35 @@ class Element:
     def __init__( self, name, decays ):
         self.name = name
         self.halftimes = np.zeros(len(decays))
-        products = { "beta" : [], "gamma" : [] }
+        self.products = { "beta" : [[],[],[]], "gamma" : [[],[],[]], "EC gamma" : [[],[],[]] }
         
         for i in range(len(decays)):
             h = decays[i][0].split(' ')
             self.halftimes[i] = float(h[0]) * time_units[h[1]]
-            for p in decays[i][1:]:
-                products[p]
+            for p in decays[i][1]:
+                productlist = self.products[p[0]]
+                for energy, sig_energy, intensity in p[2]:
+                    productlist[0].append( energy )
+                    productlist[1].append( sig_energy )
+                    productlist[2].append( p[1]*intensity/self.halftimes[i] )
 
+        for key in self.products:
+            self.products[key] = np.array( self.products[key] )
+            self.products[key][2] /= np.sum(1/self.halftimes)
+
+    def Read( filename ):
+        data = json.loads( open( filename ).read() )
+        elements = [ Element( name, decays ) for name, *decays in data ]
+        return elements
+
+    def __repr__(self):
+        ret = self.name + "\n\thalftimes: "
+        for h in self.halftimes[:-1]:
+            ret += str(float(h))+", "
+        ret += str(float(self.halftimes[-1])) + "\n"
+        ret += "Products:"
+        for key in self.products:
+            ret += "\n\t"+key + ": "
+            for p in self.products[key].T:
+                ret += str({"E":p[0], "sig_E":p[1], "I":p[2]}) + ", "
+        return ret
