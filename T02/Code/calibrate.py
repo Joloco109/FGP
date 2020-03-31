6e846f6bad639b705b892b95f89e6364d950c48a
Jonas Colve <jonas.colve@outlook.de>
2020-03-31 18:54:51 +0200

    WIP version of finder, still not ready after hours of work

---
+++ T02/Code/calibrate.py
@@ -0,0 +1,19 @@
+from calibration import Calibration
+from histogram import Histogram
+import config as cfg
+from finder import find_edges
+
+def calibrate():
+    opt_rausch, rausch = Histogram.Read( cfg.cali_dir+cfg.cali_rausch, "Rauschmessung", "Rauschmessung" )
+
+    for f in cfg.cali_files:
+        opt, hist = Histogram.Read( cfg.cali_dir+f, f[:-4], f[:-4] )
+        hist -= opt.time / opt_rausch.time * rausch
+
+        edges = find_edges( hist, 10, 6, 40 )
+
+        hist.Draw()
+        input()
+
+if __name__ == "__main__":
+    calibrate()
---
+++ T02/Code/finder.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def slope( x, y, w ):
+    sumw2 = np.sum( w )
+    if sumw2 >= np.inf:
+        mask = tuple([w >= np.inf])
+        x = x[mask]
+        y = y[mask]
+        sumw2 = 1
+        w = np.ones(len(x))/len(x)
+
+    x_bar = np.sum( w*x )/sumw2
+    x2_bar = np.sum( w*(x**2) )/sumw2
+    if not x2_bar == x_bar**2:
+        m = np.sum( w*(x-x_bar)*y)/sumw2 /(x2_bar - x_bar**2)
+    else:
+        m = 0
+    return m
+
+
+def find_edges( hist, acc, width, degree ):
+    i = 0
+    N = hist.GetN()
+
+    centers = hist.GetBinCenters()
+    centers /= np.max(centers)
+    contents = hist.GetBinContents()
+    errors = hist.GetBinErrors()
+
+    max_cont = np.sum(np.sort(contents)[-10:-2])/8
+    errors /= max_cont
+    contents /= max_cont
+
+    with np.errstate(divide='ignore'):
+        weights = 1/errors**2
+
+    while i < len(weights) - width - acc:
+        m_last = slope( centers[i-acc:i], contents[i-acc:i], weights[i-acc:i] )
+        m = slope( centers[i+width:i+width+acc], contents[i+width:i+width+acc], weights[i+width:i+width+acc] )
+        if np.arcsin( m ) - np.arcsin( m_last ) > degree/180*np.pi:
+            print( "Rising at {}: from {:.3e}({:.2f}°) to {:.3e}({:.2f}°)".format(
+                i,
+                m_last, 180/np.pi*np.arcsin(m_last),
+                m, 180/np.pi*np.arcsin(m)  )  )
+            #i += acc
+        if np.arcsin( m ) - np.arcsin( m_last ) < -degree/180*np.pi:
+            print( "Sinking at {}: from {:.3e}({:.2f}°) to {:.3e}({:.2f}°)".format(
+                i, 180/np.pi*m_last, np.arcsin(m_last),
+                m, 180/np.pi*np.arcsin(m)  )  )
+            #i += acc
+        i += 1
+    return max_cont
