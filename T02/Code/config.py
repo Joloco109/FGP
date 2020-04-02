1a59fd79f1c8f8ee844347096b8b03ba59bbbbf6
Jonas Colve <jonas.colve@outlook.de>
2020-04-02 15:06:24 +0200

    Made another stupid mistake this time in config.py

--- T02/Code/config.py
+++ T02/Code/config.py
@@ -3,8 +3,8 @@ data_dir = "Data/"
 cali_dir = data_dir+"Kalibrierung/"
 
 cali_files = [
-        [ "60_Co", "137 Cs Kalibrierung.TKA" ],
-        [ "137Cs", "Co Kalibrierung.TKA" ],
+        [ "137Cs", "137 Cs Kalibrierung.TKA" ],
+        [ "60_Co", "Co Kalibrierung.TKA" ],
         [ "152Eu", "Eu Kalibrierung.TKA" ],
         [ "22Na", "Na 22 Kalibrierung.TKA" ],
     ]
