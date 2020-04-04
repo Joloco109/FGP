dce151f17ad6f36003c028755a409105ecc59ce3
Jonas Colve <jonas.colve@outlook.de>
2020-04-04 12:03:40 +0200

    Added output of peak/edge position to calibration

--- T02/Code/calibrate.py
+++ T02/Code/calibrate.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 from calibration import Calibration
 from histogram import Histogram
@@ -7,6 +8,8 @@ from finder import find_edges, peak_fit, edge_fit
 
 def calibrate_known( plot=False, out=False ):
     opt_rausch, rausch = Histogram.Read( cfg.cali_dir+cfg.cali_rausch, "Rauschmessung", "Rauschmessung" )
+    rausch.Draw()
+    input()
 
     data = json.loads( open( cfg.cali_dir+cfg.cali_extrema ).read() )
     for [element_name, candidates] in data:
@@ -28,6 +31,34 @@ def calibrate_known( plot=False, out=False ):
         comp_edges_fits = edge_fit( hist, comp_edges, right=True, plot=False, out=out )
         peak_fits = peak_fit( hist, peaks, plot=False, out=out )
 
+        back_pos = []
+        if out:
+            print("Backscatter:")
+        for f in back_edges_fits:
+            paras = f.GetParameters()
+            sig_paras = f.GetParErrors()
+            back_pos.append( ( paras[2] - paras[3], np.sqrt(sig_paras[2]**2+sig_paras[3]**2) ) ) # e = a - d
+            if out:
+                print("\t{:.2f} +- {:.2f}".format(*back_pos[-1]))
+        comp_pos = []
+        if out:
+            print("Compton scatter:")
+        for f in comp_edges_fits:
+            paras = f.GetParameters()
+            sig_paras = f.GetParErrors()
+            comp_pos.append(( paras[2] + paras[3], np.sqrt(sig_paras[2]**2+sig_paras[3]**2) )) # e = a + d
+            if out:
+                print("\t{:.2f} +- {:.2f}".format(*comp_pos[-1]))
+        peak_pos = []
+        if out:
+            print("Photopeaks:")
+        for f in peak_fits:
+            paras = f.GetParameters()
+            sig_paras = f.GetParErrors()
+            peak_pos.append(( paras[3], sig_paras[3] )) # e = [3]
+            if out:
+                print("\t{:.2f} +- {:.2f}".format(*peak_pos[-1]))
+
         if plot:
             hist.Draw()
             for f in back_edges_fits:
@@ -37,6 +68,7 @@ def calibrate_known( plot=False, out=False ):
             for f in peak_fits:
                 f.function.Draw("Same")
             input()
+
 
 def calibrate():
     opt_rausch, rausch = Histogram.Read( cfg.cali_dir+cfg.cali_rausch, "Rauschmessung", "Rauschmessung" )
