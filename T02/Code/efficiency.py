af27e05a43bf04a83ed4cb99934fcc5b3f58d6cd
Anna <anna.stollenwerk@rwth-aachen.de>
2020-04-06 16:32:21 +0200

    efficiency data finished (cali fehlt)

--- T02/Code/efficiency.py
+++ T02/Code/efficiency.py
@@ -9,26 +9,40 @@ import config as cfg
 from finder import find_edges, peak_fit, edge_fit
 
 graph_dir = "Graphs/build/efficiency/" 
+peak_area = [(380, 480)]
+element_name = "137Cs"
 
 def efficiency( plot=False, out=False, save=False ):
     opt_noise, noise = Histogram.Read( cfg.eff_dir+cfg.eff_files[0], "noise", "noise measurement" )
-    if plot:
-        canvas = TCanvas("canvas","canvas")
-        noise.Draw(xName="channel number", yName="counts")
-        if save:
-            canvas.SaveAs( graph_dir + "noise.eps" )
-        input()
+   
     opt, hist = Histogram.Read( cfg.eff_dir+cfg.eff_files[1], "eff", "efficiency measurement" )
     hist -= opt.time / opt_noise.time * noise
+    
+    peak_fits = peak_fit( hist, peak_area, plot=False, out=False)
+    for f in peak_fits:
+            paras = f.GetParameters()
+            sig_paras = f.GetParErrors()
+            if out:
+                for i,j in zip(paras, sig_paras):
+                    print(i,"\pm",j)
     if plot:
         canvas = TCanvas("canvas","canvas")
         hist.Draw(xName="channel number", yName="counts")
-        if save:
-            canvas.SaveAs( graph_dir + "efficency.eps" )
+        n = hist.hist.Integral(380, 480)
+        print("Root: ", n)
+        for f in peak_fits:
+            f.function.SetLineColor(2)
+            f.function.Draw("Same")
         input()
+        N = paras[2]*paras[4]*np.sqrt(2*np.pi)
+        N_err = np.sqrt((paras[4]*np.sqrt(2*np.pi)*sig_paras[2])**2+(paras[2]*np.sqrt(2*np.pi)*sig_paras[4])**2)
+        print("Analytic: ", N ," \pm ", N_err)
+        
+    if save:
+        canvas.SaveAs( graph_dir + "efficency.eps" ) 
+        
 if __name__ == "__main__":
     
     if not os.path.exists( graph_dir ):
         os.makedirs( graph_dir )
-
-    efficiency( True, True, True )
+    efficiency( True, True, True)
