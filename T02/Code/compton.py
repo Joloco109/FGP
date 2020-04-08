441574909ab2e77491560734b1d939739404966a
Jonas Colve <jonas.colve@outlook.de>
2020-04-08 16:50:34 +0200

    First version of compton

---
+++ T02/Code/compton.py
@@ -0,0 +1,39 @@
+from ROOT import TCanvas
+
+import config as cfg
+from histogram import Histogram
+from calibrate import calibrate_known
+
+cali = calibrate_known()
+
+def analyse_spectrum( name, file_name, noise_file ):
+    canvas = TCanvas(name.format(""))
+    canvas.Divide(1,2)
+
+    opt_noise, noise = Histogram.Read( noise_file, name.format("Noise "), name.format("Noise "), calibration=cali )
+    opt_hist, hist_raw = Histogram.Read( file_name, name.format("Spectrum "), name.format("Spectrum "), calibration=cali )
+    canvas.cd(1)
+    hist_raw.Draw()
+
+    hist = hist_raw - opt_hist.time/opt_noise.time * noise
+    canvas.cd(2)
+    hist.Draw()
+    input()
+
+def diff_cross_section( name, angles, noise, ring ):
+    if ring:
+        for angle in angles:
+            r, l, n, file_name = angles[angle]
+            analyse_spectrum( "\\mbox{{{}"+name+" }}" + "{:.0f}^\\circ".format(angle[0]),
+                    cfg.comp_r_dir+file_name, cfg.comp_r_dir+noise[l[0]] )
+
+    else:
+        for angle in angles:
+            file_name = angles[angle]
+            analyse_spectrum( "\\mbox{{{}"+name+" }}" + "{:.0f}^\\circ".format(angle),
+                    cfg.comp_c_dir+file_name, cfg.comp_c_dir+noise[angle] )
+
+if __name__=="__main__":
+    diff_cross_section( "Conventional Alu", cfg.conv_files_Alu, cfg.conv_noise, False  )
+    diff_cross_section( "Conventional Steel", cfg.conv_files_Steel, cfg.conv_noise, False  )
+    diff_cross_section( "Ring", cfg.ring_files, cfg.ring_noise, True )
